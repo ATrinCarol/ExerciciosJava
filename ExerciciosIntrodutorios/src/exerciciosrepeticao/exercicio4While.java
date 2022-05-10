32fe2dbc396cbfa0cc0ab5ee6aa26e5ae8b76026
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-05-10 15:23:08 -0300

    add exemplos de arrays

--- ExerciciosIntrodutorios/src/exerciciosrepeticao/exercicio4While.java
+++ ExerciciosIntrodutorios/src/exerciciosrepeticao/exercicio4While.java
@@ -1,6 +1,6 @@
 package exerciciosrepeticao;
 
-import java.lang.String;
+import java.util.*;
 
 /*    4- Uma empresa desenvolveu uma pesquisa para saber as características psicológicas dos 
  * indivíduos de uma região. Para tanto, a cada uma das pessoas era perguntado: 
@@ -15,21 +15,61 @@ import java.lang.String;
     • o número de pessoas nervosas com mais de 40 anos; 
     • o número de pessoas calmas com menos de 18 anos.*/
 
+//EXEMPLO DO AUGUSTO ABAIXO 
+
 public class exercicio4While {
 
 	public static void main(String[] args) {
 		
-		int idade; 
-		String nome = new String ();
-		String sexo = new String();
-		String opcao1 = "pessoa calma";
-		String opcao2 = "pessoa nervosa";
-		String opcao3 = "pessoa agressiva";
+		Scanner input = new Scanner(System.in);
+		int escolhaSexo, escolhaCarac;
+		int qntPessoa = 1, idade;
+		int numCal = 0, numMulNer = 0, numHomAgr=0, numOutCal=0, numNerMaior40=0, numCalMenor18=0;
 		
-		while (opcao1==nome) { //VOU CONCERTAR PORQUE ESTÁ BEM BAGUNÇADO. SÓ COLOQUEI ISSO PRA NÃO FICAR COM ERRO
+		while(qntPessoa <= 150)
+		{
+			do
+			{
+				System.out.print("Digite sua idade: ");
+				idade = input.nextInt();
+				if(idade<0)
+					System.out.println("Idade invalida, digite idade maior ou igual a 0");
+					
+			}while(idade < 0);
 			
+			do
+			{
+				System.out.println("Digite a escolha de sexo (1-feminino / 2-masculino / 3-Outros) ");
+				escolhaSexo = input.nextInt();
+				if(escolhaSexo <= 0 || escolhaSexo > 3)
+					System.out.println("Valor invalido, digite valor valido");
+			}while(escolhaSexo <= 0 || escolhaSexo > 3);
+			
+			do
+			{
+				System.out.println("Digite a escolha da caracteristica (1-calma / 2-nervosa / 3-agressiva). ");
+				escolhaCarac = input.nextInt();
+				if(escolhaCarac <= 0 || escolhaCarac > 3)
+					System.out.println("Valor invalido, digite valor valido");
+			}while(escolhaCarac <= 0 || escolhaCarac > 3);
+			
+			if(escolhaCarac == 1)
+				numCal += 1;
+			if(escolhaSexo == 1 && escolhaCarac == 2)
+				numMulNer =+1;
+			if(escolhaSexo == 2 && escolhaCarac == 3)
+				numHomAgr +=1;
+			if(escolhaSexo == 3 && escolhaCarac == 1)
+				numOutCal +=1;
+			if(idade > 40 && escolhaCarac == 2)
+				numNerMaior40 +=1;
+			if(idade < 18 && escolhaCarac == 1)
+				numCalMenor18 +=1;
+			qntPessoa += 1;
 		}
 		
+		System.out.println("O numero de pessoas calmas são: "+numCal+"\nO numero de mulheres nervosas são: "+numMulNer+"\nO numero de homens agressivos são: "+numHomAgr+
+				"\nO numero de outros calmos são: "+numOutCal+"\nO numero de pessoas nervosas com mais de 40 anos são: "+numNerMaior40+"\nO numero de pessoas calmas com menos de 18 anos são: "+numCalMenor18);
 
 	}
 
